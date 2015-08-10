fc3f23be3801be8c98e9bb9448e5097568ce7925
anorudes <anorudes@gmail.com>
2015-08-10 15:24:06 +0500

    fix

--- src/scripts/Reducers/Reducers.js
+++ src/scripts/Reducers/Reducers.js
@@ -2,13 +2,12 @@ import { SETSHOPS } from '../Actions/Actions';
 import shopsData from './Shops.js';
 
 export function shops(state = shopsData, action) {
-  let newState = state;
-
+  let newState = [...state];
+  
   switch (action.type) {
   case SETSHOPS:
     newState.push(action.shop);
-    console.log(newState)
-    return newState
+  return newState;
   default:
     return state;
   }
