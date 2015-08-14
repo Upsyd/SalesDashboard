969b54bbe370fe419608a8d8f1e547c80f751b9e
keske <hello@andreykeske.com>
2015-08-14 14:30:19 +0300

    package

--- src/scripts/Components/Shops.js
+++ src/scripts/Components/Shops.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import { createSelector } from 'reselect';
-
 import _ from 'underscore';
 
 // Actions
--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,10 @@ module.exports = {
     new ExtractTextPlugin('bundle.css'),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.NoErrorsPlugin(),
+    new webpack.ProvidePlugin({
+      jQuery: "jquery"
+    })
   ],
 
   resolve: {
@@ -32,32 +35,31 @@ module.exports = {
 
   module: {
     loaders: [{
-        test: /bootstrap\/js\//,
-        loader: 'imports?jQuery=jquery'
-      }, {
-        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "url?limit=10000&minetype=application/font-woff"
-      }, {
-        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "url?limit=10000&minetype=application/font-woff2"
-      }, {
-        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "url?limit=10000&minetype=application/octet-stream"
-      }, {
-        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "file"
-      }, {
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "url?limit=10000&minetype=image/svg+xml"
-      }, {
-        test: /\.js$/,
-        loaders: ['react-hot', 'babel-loader?optional[]=runtime&stage=0'],
-        exclude: /node_modules/
-      }, {
-        test: /\.scss$/,
-        loader: "css!postcss-loader!sass"
-      }
-    ],
+      test: /bootstrap\/js\//,
+      loader: 'imports?jQuery=jquery'
+    }, {
+      test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+      loader: "url?limit=10000&minetype=application/font-woff"
+    }, {
+      test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+      loader: "url?limit=10000&minetype=application/font-woff2"
+    }, {
+      test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+      loader: "url?limit=10000&minetype=application/octet-stream"
+    }, {
+      test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+      loader: "file"
+    }, {
+      test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+      loader: "url?limit=10000&minetype=image/svg+xml"
+    }, {
+      test: /\.js$/,
+      loaders: ['react-hot', 'babel-loader?optional[]=runtime&stage=0'],
+      exclude: /node_modules/
+    }, {
+      test: /\.scss$/,
+      loader: "css!postcss-loader!sass"
+    }]
   },
   postcss: function() {
     return [autoprefixer, csswring];
